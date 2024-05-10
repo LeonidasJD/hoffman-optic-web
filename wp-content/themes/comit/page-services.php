07087d9f01aa65e0b07be8398528298645828967
LeonidasJD <milandjordjevic633@gmail.com>
2024-05-10 15:16:29 +0200

    created services page

---
+++ wp-content/themes/comit/page-services.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+<section class="services-section-wrapper">
+    <div class="services-section-under-wrapper">
+        <h1>We accompany you and we educate in your process, whatever be the objective.</h1>
+    </div>
+</section>
+<?php get_footer(); ?>
