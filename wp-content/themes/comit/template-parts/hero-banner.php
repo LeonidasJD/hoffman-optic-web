2dd35802646f097cc3f0e6e8c21118723964d3ff
LeonidasJD <milandjordjevic633@gmail.com>
2024-05-11 14:19:16 +0200

    crating hero banner template part

--- wp-content/themes/comit/template-parts/hero-banner.php
+++ wp-content/themes/comit/template-parts/hero-banner.php
@@ -1,5 +1,8 @@
 <!--GREEN HERO BANNER TOP PAGE-->
-<div class="hero-banner-wrapper">
-    <h1><?php echo get_the_title(); ?></h1>
+<div class="hero-banner-wrapper" style="background-image: url('<?php echo get_the_post_thumbnail_url(); ?>');">
+<div class="overlay">
+<h1><?php echo get_the_title(); ?></h1>
+</div>
+    
 
 </div>
