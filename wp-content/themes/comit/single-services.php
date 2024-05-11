1324f3444ecb6748e2a1bcf6a2b15b951339452f
LeonidasJD <milandjordjevic633@gmail.com>
2024-05-11 14:09:21 +0200

    added a form

--- wp-content/themes/comit/single-services.php
+++ wp-content/themes/comit/single-services.php
@@ -3,5 +3,17 @@
 <section class="services-post-hero-wrapper">
 <?php  get_template_part('template-parts/hero-banner'); ?>
 </section>
-<!--HERO SECTION START-->
+<!--HERO SECTION END-->
+
+<!--TEXT AND FORM SECTION START-->
+<section class="text-form-section-wrapper">
+    <div class="text-form-section-underwrapper">
+        <div class="post-text"><h2>text placeholder</h2></div>
+        <div class="post-form"><?php echo FrmFormsController::get_form_shortcode( array( 'id' => 3 ) ); ?></div>
+    </div>
+</section>
+<!--TEXT AND FORM SECTION END-->
+
+
 <?php get_footer();?>
+
