457088552c488597f92eb1def142ac39d5b0e88e
LeonidasJD <milandjordjevic633@gmail.com>
2024-05-11 12:49:40 +0200

    creating single services page

---
+++ wp-content/themes/comit/single-services.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+<!--HERO SECTION START-->
+<section class="services-post-hero-wrapper">
+<?php  get_template_part('template-parts/hero-banner'); ?>
+</section>
+<!--HERO SECTION START-->
+<?php get_footer();?>
