ddf74971634599344176cf43891dc3c7d25bb3a1
LeonidasJD <milandjordjevic633@gmail.com>
2024-05-12 18:00:34 +0200

    creating logic for service section

--- wp-content/themes/comit/single-services.php
+++ wp-content/themes/comit/single-services.php
@@ -8,7 +8,28 @@
 <!--TEXT AND FORM SECTION START-->
 <section class="text-form-section-wrapper">
     <div class="text-form-section-underwrapper">
-        <div class="post-text"><h2>text placeholder</h2></div>
+        <div class="post-text">
+            <?php /**prolazim kroz repeater i proveravam da li ima sekcija u single service postu */
+            if(have_rows('service_section')):
+                 while(have_rows('service_section')) :
+                     the_row();
+                
+                     $section_title = get_sub_field('title');
+
+                     if(!empty($section_title)){
+                        ?>
+                        <div class="services-section-title"><?php echo $section_title ?></div>
+                        <?php
+                     }
+
+            endwhile;
+            else:
+                echo 'No services found';
+        endif;
+      
+            ?>
+            <!--kraj petlje i ispisivanje sekcija -->
+        </div>
         <div class="post-form"><?php echo FrmFormsController::get_form_shortcode( array( 'id' => 3 ) ); ?></div>
     </div>
 </section>
