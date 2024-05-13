cfa6477c02ce18dc6113ef8b4dac84310f75d78b
LeonidasJD <milandjordjevic633@gmail.com>
2024-05-13 15:25:02 +0200

    crateing about us page

---
+++ wp-content/themes/comit/page-about-us.php
@@ -0,0 +1,57 @@
+<?php get_header(); ?>
+<!--HERO SECTION START-->
+<section class="about-us-hero-wrapper">
+    <div class="about-us-hero-underwrapper">
+        <h1>Lorem ipsum dolor sit amet consectetur  consequat vel vivamus tellus.</h1>
+    </div>
+</section>
+<!--HERO SECTION END-->
+
+<!--OUR MISSION SECTION START-->
+<section class="our-mission-section-wrapper">
+    <div class="our-mission-section-underwrapper">
+        <div class="data-section-info">
+            <h2>Our mission is your satisfaction.</h2>
+            <p>The Hoffmannoptik team is available to answer any questions you may have about vision, your eye health and the latest eyewear fashions.<br><br>All employees are trained opticians or master opticians who practice their profession with the utmost precision and passion. Each pair of glasses is manufactured with skilled craftsmanship and the highest level of quality awareness, making them unique.</p>
+        </div>
+        <div class="data-section-info">
+            <h2>Holistic eye optics.</h2>
+            <p>In our optical competence center, highly qualified optometrists are available for a holistic examination of your eyes. We offer you a careful eyeglass determination from a holistic perspective. Our focus is on analyzing visual perception and improving it through glasses, contact lenses or magnifying visual aids (magnifying glasses). Our opticians will be happy to advise you on current eyewear fashions, the right lenses, sunglasses and contact lenses for you.<br><br> Our eye care program expands the eyeglass assessment to include comprehensive optometric screening. The aim is to identify possible eye defects that are relevant to the prescription, as well as any abnormalities in the eye that require an additional specialist medical examination. Our interdisciplinary collaboration enables us to contribute to the holistic eye health of our customers.</p>
+        </div>
+    </div>
+</section>
+<!--OUR MISSION SECTION END-->
+
+<!--CORE OF OUR WORK  SECTION START-->
+<section class="our-work-section-wrapper">
+    <div class="our-work-section-underwrapper">
+        <div class="our-work-section-heading-wrapper"><h2>The core of our work:<br>The 4 values ​​of holistic eye optics</h2></div>
+        <div class="our-work-section-data-wrapper">
+        <div class="our-work-data" >
+            <img src="/wp-content/uploads/2024/05/ion_eye-outline.png">
+            <h3>Good vision from a single source</h3>
+            <p>From the eye exam to the finished pair of glasses – the focus of our philosophy is the closely coordinated collaboration between our optometrists and opticians.</p>
+        </div>
+        <div class="our-work-data" >
+            <img src="/wp-content/uploads/2024/05/solar_medal-star-outline.png">
+            <h3>Qualifications, experience & quality awareness</h3>
+            <p>Due to their in-depth training and many years of experience, our optometrists can help with a variety of visual defects and vision problems and provide you with the best possible care with contact lenses and glasses.</p>
+        </div>
+        <div class="our-work-data" >
+            <img src="/wp-content/uploads/2024/05/ri_heart-2-line.png">
+            <h3>Service is our passion</h3>
+            <p>Our heart beats for the health trade of optics, your satisfaction is our top priority. Our offers also include numerous free services for your glasses and contact lenses.</p>
+        </div>
+        <div class="our-work-data" >
+            <img src="/wp-content/uploads/2024/05/mdi_partnership-outline.png">
+            <h3>Successful with strong partners</h3>
+            <p>With lenses from Essilor and Hoya, as well as contact lenses from Hecht, Wöhlk, Bausch and Lomb, we rely on the highest quality and innovation. In our range of glasses you will always find a large selection from the current collections of well-known eyewear manufacturers. Tradition - Innovation - Partnership</p>
+        </div>
+        </div>
+        
+        
+    </div>
+</section>
+<!--CORE OF OUR WORK  SECTION END-->
+
+<?php get_footer(); ?>
