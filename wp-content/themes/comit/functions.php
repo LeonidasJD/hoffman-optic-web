85b3a476119d7ffbedcf9369b8cae50720f4ec03
LeonidasJD <milandjordjevic633@gmail.com>
2024-05-14 16:20:17 +0200

    added jquery

--- wp-content/themes/comit/functions.php
+++ wp-content/themes/comit/functions.php
@@ -143,6 +143,8 @@ function comit_scripts() {
 
 	wp_enqueue_script( 'comit-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 	wp_enqueue_style( 'comit-style-home', get_template_directory_uri() . '/inc/css/homepage.css', array(), _S_VERSION, true );
+	wp_deregister_script('jquery');
+    wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js', array(), null, true);
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
