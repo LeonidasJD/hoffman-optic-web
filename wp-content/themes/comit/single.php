6a25da12fc46ccee6327a124128872dcafe69745
LeonidasJD <milandjordjevic633@gmail.com>
2024-05-16 09:31:54 +0200

    added single post page

--- wp-content/themes/comit/single.php
+++ wp-content/themes/comit/single.php
@@ -1,6 +1,146 @@
 <?php
 get_header();
 ?>
-<h1>test single post</h1>
-	
+<!--SINGLE POST INFO START-->
+<section class="single-post-info-wrapper">
+	<div class="single-post-info-underwrapper">
+		<div class="single-post-info">
+		<?php /**prolazim kroz repeater i proveravam da li ima sekcija u single service postu */
+            if(have_rows('news_section')):
+
+				
+				?> 
+				<div class="post-date">
+				<img src="/wp-content/uploads/2024/05/22.png">
+				<p><?php echo get_the_date(); ?></p>
+				</div>
+				
+				
+				<?php
+				
+
+                 while(have_rows('news_section')) :
+                     the_row();
+                
+                     $post_section_title = get_sub_field('post_section_title');
+                     $post_section_subtitle = get_sub_field('post_section_subtitle');
+                     $post_section_description = get_sub_field('post_section_description');
+                     $post_section_image = get_sub_field('post_section_image');
+
+					
+
+                     echo '<div class="post-section-wrapper">';
+
+					
+
+                     if(!empty($post_section_title)){
+                        ?>
+                        <div class="post-single-section-title">
+							<h2><?php echo $post_section_title  ?></h2>
+							
+						</div>
+                        <?php
+                     }
+                     if(!empty($post_section_subtitle)){
+                        ?>
+                        <div class="post-section-subtitle"><h3><?php echo $post_section_subtitle ?></h3></div>
+                        <?php
+                     }
+                     if(!empty($post_section_description)){
+                        ?>
+                        <div class="post-section-description"><p><?php echo $post_section_description ?></p></div>
+                        <?php
+                     }
+                     if(!empty($post_section_image)){
+                        ?>
+                        <div class="post-section-image"><img src="<?php echo $post_section_image ?>"></div>
+                        <?php
+                     }
+
+                     echo '</div>';
+
+            endwhile;
+            else:
+                echo '<div class="no-post-info-message"><h4>Information will be available soon</h4></div>';
+        endif;
+      
+            ?>
+            <!--kraj petlje i ispisivanje sekcija -->
+		</div>
+
+		 <!--SIDE BANNER START-->
+		<div class="single-post-side">
+		<?php  get_template_part('template-parts/latest-post'); ?>
+		</div>
+		<!--SIDE BANNER START-->
+	</div>
+</section>
+<!--SINGLE POST INFO END-->
+
+
+
+<!--RELATED POSTS START-->
+<section class="related-posts-wrapper">
+   <div class="related-posts-underwrapper">
+    <div class="related-heading-and-paragraph">
+    <h2>RELATED BLOGS</h2>
+		<p>Dignissim massa duis eget turpis fringilla nam ridiculus ultricies urna vehicula fusce. Elit interdum urna iaculis leo sit pharetra vel. Vitae letius luctus lacinia rhoncus penatibus per.</p>
+    </div>
+		
+   <div class="related-news-cards-wrapper">
+
+      <?php 
+         $current_news_id = get_the_ID();
+         $related_news_card_args= array(
+         'post_type' =>'post',
+         'posts_per_page' =>'2',
+         'order' => 'ASC',
+         'post__not_in' => array($current_news_id),
+         );
+
+         $related_news = new WP_Query( $related_news_card_args );
+
+         if($related_news->have_posts()){
+         while($related_news->have_posts()){
+         $related_news->the_post();
+         ?> 
+         <a href="<?php echo get_permalink(); ?>">
+         <div class="related-single-news-card">
+                <div class="related-card-img">
+                <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+                </div>
+                
+               <div class="related-news-card-info">
+                  <p class="single-news-date"><img src="/wp-content/uploads/2024/05/22.png"><?php echo get_the_date(); ?></p>
+                  <div class="single-card-heading-excerpt">
+                  <h2><?php echo get_the_title(); ?></h2>
+               <p><?php echo get_the_excerpt(); ?></p>
+                  </div>
+              
+               </div>
+               
+            </div></a>
+         
+         
+            
+         <?php
+            }
+         }else{
+            echo 'Nema pronađenih srodnih postova!';
+         }
+      
+      ?>
+
+   </div>
+</div>
+</section>
+
+<!--RELATED POSTS END-->
+
+
+<!--BANNER SECTION START-->
+<section class="contact-us-banner-wrapper">
+<?php  get_template_part('template-parts/contact-us-green'); ?>
+</section>
+<!--BANNER SECTION END-->
 <?php get_footer();?>
