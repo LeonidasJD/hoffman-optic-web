d04bf4fce185a82e9d097800741f3c38b5552c3d
LeonidasJD <milandjordjevic633@gmail.com>
2024-05-16 15:27:49 +0200

    added route

--- wp-content/themes/comit/template-parts/contact-us-green.php
+++ wp-content/themes/comit/template-parts/contact-us-green.php
@@ -2,6 +2,6 @@
 
 <div class="contact-us-banner-underwrapper">
     <h2>If you have questions, feel free to contact us anytimet</h2>
-    <span><a href="#">Contact us</a> <img src="/wp-content/uploads/2024/05/bi_send-fill.png" alt=""></span>
+    <span><a href="/contact-us/">Contact us</a> <img src="/wp-content/uploads/2024/05/bi_send-fill.png" alt=""></span>
       
 </div>
