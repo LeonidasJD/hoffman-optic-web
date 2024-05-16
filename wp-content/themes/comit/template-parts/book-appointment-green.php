03ecec5e143575e5066ce79c6b86fadf992733b1
LeonidasJD <milandjordjevic633@gmail.com>
2024-05-16 15:27:35 +0200

    added template part

---
+++ wp-content/themes/comit/template-parts/book-appointment-green.php
@@ -0,0 +1,7 @@
+<!-- book-appointment-green template part-->
+
+<div class="contact-us-banner-underwrapper">
+    <h2>Your Vision Matters - Secure Your Appointment Today.</h2>
+    <span><a href="#">Book your appointment</a> <img src="/wp-content/uploads/2024/05/🦆-icon-_clock-outline_.png" alt=""></span>
+      
+</div>
