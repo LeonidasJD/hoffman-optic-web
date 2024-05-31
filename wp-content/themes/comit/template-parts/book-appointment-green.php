af48bd12d41c205af84113b5a664c92d01bcc391
LeonidasJD <milandjordjevic633@gmail.com>
2024-05-31 17:20:45 +0200

    created new button

--- wp-content/themes/comit/template-parts/book-appointment-green.php
+++ wp-content/themes/comit/template-parts/book-appointment-green.php
@@ -2,6 +2,24 @@
 
 <div class="contact-us-banner-underwrapper">
     <h2>Your Vision Matters - Secure Your Appointment Today.</h2>
-    <span><a href="#">Book your appointment</a> <img src="/wp-content/uploads/2024/05/🦆-icon-_clock-outline_.png" alt=""></span>
-      
+    <!-- <span><a href="#">Book your appointment</a> <img src="/wp-content/uploads/2024/05/🦆-icon-_clock-outline_.png" alt=""></span> -->
+    <button id="contact-button" class="contact-btn">
+  <div class="svg-wrapper-1">
+    <div class="svg-wrapper">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+      >
+        <path fill="none" d="M0 0h24v24H0z"></path>
+        <path
+          fill="currentColor"
+          d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"
+        ></path>
+      </svg>
+    </div>
+  </div>
+  <span>Book your appointment</span>
+</button>
 </div>
