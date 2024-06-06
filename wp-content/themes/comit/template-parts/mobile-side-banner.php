a577e7b44feef781d43e4cc51064ebeb5aec3877
LeonidasJD <milandjordjevic633@gmail.com>
2024-06-06 17:02:02 +0200

    added href

--- wp-content/themes/comit/template-parts/mobile-side-banner.php
+++ wp-content/themes/comit/template-parts/mobile-side-banner.php
@@ -1,7 +1,7 @@
 <div class="mobile-side-banner-wrapper">
     <div class="mobile-side-banner-info"><p>Einen Termin vereinbaren
 </p></div>
-    <div class="mobile-side-banner-btn"><a href="#">Book it <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <div class="mobile-side-banner-btn"><a href="/contact-us/">Book it <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M6.66211 3.56879V7.65178H10.7451" stroke="white" stroke-width="1.30097" stroke-linecap="round" stroke-linejoin="round"/>
 <path d="M7.30582 12.6116C10.5123 12.6116 13.1116 10.0123 13.1116 6.80582C13.1116 3.59935 10.5123 1 7.30582 1C4.09935 1 1.5 3.59935 1.5 6.80582C1.5 10.0123 4.09935 12.6116 7.30582 12.6116Z" stroke="white" stroke-width="1.30097" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
