5061b06d398616228a1b580337ddb23e51410cf7
LeonidasJD <milandjordjevic633@gmail.com>
2024-06-11 14:44:38 +0200

    styled

--- wp-content/themes/comit/template-parts/book-appointment-green.php
+++ wp-content/themes/comit/template-parts/book-appointment-green.php
@@ -8,7 +8,7 @@
     <div class="svg-wrapper">
     <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M11.0713 5.86719V13.603H18.8071" stroke="#36641C" stroke-width="1.80097" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M12.2935 23C18.3685 23 23.2935 18.0751 23.2935 12C23.2935 5.92487 18.3685 1 12.2935 1C6.21832 1 1.29346 5.92487 1.29346 12C1.29346 18.0751 6.21832 23 12.2935 23Z" stroke="#76BA51" stroke-width="1.80097" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M12.2935 23C18.3685 23 23.2935 18.0751 23.2935 12C23.2935 5.92487 18.3685 1 12.2935 1C6.21832 1 1.29346 5.92487 1.29346 12C1.29346 18.0751 6.21832 23 12.2935 23Z" stroke="#36641C" stroke-width="1.80097" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
     </div>
   </div>
