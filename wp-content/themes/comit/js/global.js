6646f941f0bf1316bbdbb1ca129b755d24c96a15
LeonidasJD <milandjordjevic633@gmail.com>
2024-06-18 16:30:59 +0200

    added script for sticky button

--- wp-content/themes/comit/js/global.js
+++ wp-content/themes/comit/js/global.js
@@ -1,5 +1,7 @@
-//STICY BUTTON TO TOP START
+//STICY BUTTON BACK TO TOP START
+var stickyButtonWrappers = document.getElementsByClassName('sticky-button-wrapper');
 var stickyButton = document.getElementById('sticky-btn');
+
 
 stickyButton.addEventListener('click', function(){
     window.scrollTo({
@@ -7,6 +9,25 @@ stickyButton.addEventListener('click', function(){
         behavior: 'smooth'
     });
 });
-//STICY BUTTON TO TOP END
+
+
+window.addEventListener('scroll', function(){
+    if(window.scrollY > 400){
+       for (stickyButtonWrapper of stickyButtonWrappers){
+        stickyButtonWrapper.style.visibility = 'visible';
+        stickyButtonWrapper.style.opacity = '1';
+       }
+    }else{
+        for (stickyButtonWrapper of stickyButtonWrappers){
+            stickyButtonWrapper.style.visibility = 'hidden';
+            stickyButtonWrapper.style.opacity = '0';
+           }
+    }
+});
+
+
+//STICY BUTTON BACK TO TOP END
+
+
 
 
