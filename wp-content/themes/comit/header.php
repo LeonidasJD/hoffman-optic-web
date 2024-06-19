2ce6315927a7371ceb2f99a130fb54d44215d239
LeonidasJD <milandjordjevic633@gmail.com>
2024-06-19 15:21:55 +0200

    space

--- wp-content/themes/comit/header.php
+++ wp-content/themes/comit/header.php
@@ -37,7 +37,6 @@
   rel="stylesheet"
   href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
 />
-
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 <!--SWIPER JS END-->
 
