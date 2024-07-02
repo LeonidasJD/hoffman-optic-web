92fbfa07fe1c1144f5ff09316df56eae93de545b
LeonidasJD <milandjordjevic633@gmail.com>
2024-07-02 16:03:40 +0200

    created template

---
+++ wp-content/themes/comit/page-templates/template-datenschutz.php
@@ -0,0 +1,142 @@
+<?php
+//Template Name: Datenschutz
+get_header();
+?>
+
+<!-- MAIN TITLE SECTION START -->
+<div class="datenschutz-container">
+    <div class="container-14">
+        <div class="datenschutz-wrapper">
+            <h1>Datenschutzerklärung</h1>
+            <p>Datenschutzerklärung für Webseitennutzer von Hofmannoptik e. K.</p>
+        </div>
+    </div>
+</div>
+<div class="daten-divider"></div>
+<!-- MAIN TITLE SECTION END -->
+
+<!-- TEXT SECTION START -->
+ <div class="datenschutz-container">
+    <div class="container-14">
+        <div class="text-section-wrapper">
+            <div class="daten">
+            <h2>1 Information über die Erhebung personenbezogener Daten</h2>
+            <h3>1 Allgemein Informationen</h3>
+            <p>Wir freuen uns über Ihr Interesse an unserer Webseite. Der Schutz Ihrer personenbezogenen Daten an-lässlich Ihres Besuchs auf unserer Homepage hat einen besonders hohen Stellenwert für die Geschäftslei-tung von Hoffmannoptik e.K. Mit den nachfolgenden Informationen geben wir Ihnen einen Überblick über die Verarbeitung Ihrer personenbezogenen Daten durch uns und Ihren Rechten aus dem Datenschutz. Personenbezogene Daten sind alle Daten, die auf Sie persönlich beziehbar sind, z. B. Name, Adresse, E-Mail-Adressen, Nutzerverhalten.<br><br>
+Sofern eine betroffene Person besondere Services unseres Unternehmens über unsere Internetseite in Anspruch nehmen möchte, wie z. B. unser Kontaktformular, könnte eine Verarbeitung personenbezogener Daten erforderlich werden. Ist die Verarbeitung personenbezogener Daten erforderlich und besteht für eine solche Verarbeitung keine gesetzliche Grundlage, holen wir generell eine Einwilligung der betroffenen Personen ein. Die Verarbeitung erfolgt dabei stets im Einklang mit der EU-Datenschutz-Grundverordnung (DSG-VO) und in Übereinstimmung mit den für Hoffmannoptik e.K. geltenden landesspezifischen Datenschutzbestimmungen.<br><br>
+Hoffmannoptik e.K. hat als für die Verarbeitung Verantwortlicher technische und organisatorische Maßnahmen umgesetzt, um einen möglichst lückenlosen Schutz der über diese Internetseite verarbeiteten personenbezogenen Daten gegen Verlust, Zerstörung, Zugriff, Veränderung oder Verbreitung Ihrer Daten durch unbefugte Personen sicherzustellen. Hierzu zählt auch das Ihre persönlichen Daten bei uns sicher durch Verschlüsselung übertragen werden. Wir bedienen uns dabei des Codierungssystems TSL (Transport Layer Security).<br><br>
+Dennoch kann ein absoluter Schutz aufgrund von grundsätzlichen Sicherheitslücken bei internetbasierten Datenübertragungen nicht gewährleistet werden.
+</p>
+            </div>
+
+            <div class="daten">
+            <h3> 2 Verantwortlicher für die Datenverarbeitung</h3>
+            <p>Verantwortlicher gem. Art. 4 Abs. 7 EU-Datenschutz-Grundverordnung (DS-GVO) und den geltenden landesspezifischen Datenschutzbestimmungen ist:<br><br>
+                Hoffmannoptik e.K. <br>
+                Rebstraße 4, D-79395 Neuenburg am Rhein, Telefon 07631-73606, <br>
+                eMail: neuenburg@hoffmann-optik.de<br><br>
+                Sie erreichen unseren Datenschutzbeauftragten per Post unter der o.g. Adresse mit dem Zusatz – Datenschutzbeauftragter – oder per E-Mail unter:
+                neuenburg@hoffmann-optik.de
+
+</p>
+            </div>
+
+            <div class="daten">
+            <h3>3 Allgemeines zur Datenverarbeitung</h3>
+            <p>Personenbezogene Daten unserer Nutzer erheben und verwenden wir nur, soweit dies zur Bereitstellung einer funktionsfähigen Website sowie zur Darstellung unserer Inhalte und Erbringung von Leistungen erforderlich ist. Die Erhebung und Verwendung personenbezogener Daten unserer Nutzer erfolgt regelmäßig nur nach Einwilligung des Nutzers. Eine Ausnahme gilt in solchen Fällen, in denen eine vorherige Einholung einer Einwilligung aus tatsächlichen Gründen nicht möglich ist und die Verarbeitung der Daten durch gesetzliche Vorschriften gestattet ist. Folgende Rechtsgrundlagen für die  Verarbeitung Ihrer personenbezogenen Daten kommen hierbei in Frage:<br><br>
+            <ul>
+                <li>Verarbeitung Aufgrund Ihrer Einwilligung (Artikel 6 Abs. 1 lit. a DSGVO)</li>
+                <li>Verarbeitung zur Erfüllung eines Vertrages, dessen Vertragspartei die betroffene Person ist. Dies gilt auch für Verarbeitungsvorgänge, die zur Durchführung vorvertraglicher Maßnahmen erforderlich sind (Artikel 6 Abs. 1 lit. b DSGVO)</li>
+                <li>Verarbeitung zur Erfüllung einer rechtlichen Verpflichtung, der unser Unternehmen unterliegt (Artikel 6 Abs. 1 lit. c DSGVO)</li>
+                <li>Verarbeitung Für den Fall, dass lebenswichtige Interessen der betroffenen Person oder einer an-deren natürlichen Person eine Verarbeitung personenbezogener Daten erforderlich machen (Artikel 6 Abs. 1 lit. d DSGVO)</li>
+                <li>Verarbeitung zur Wahrung eines berechtigten Interesses unseres Unternehmens oder eines Dritten, sofern nicht die Interessen, Grundrechte und Grundfreiheiten des Betroffenen das erstgenannte Interesse überwiegen (Artikel 6 Abs. 1 lit. f DSGVO). Berechtigte Interessen können insbesondere sein:
+                    <ul>
+                        <li>die Inhalte unserer Internetseite korrekt auszuliefern;</li>
+                        <li>die Inhalte unserer Internetseite korrekt auszuliefern;</li>
+                        <li>um im Falle eines Cyberangriffes die zur Strafverfolgung notwendigen Informationen den Strafverfolgungsbehörden bereitzustellen;</li>
+                        <li>die Beantwortung von Anfragen und Bereitstellung von Diensten und/oder Informationen, die für Sie bestimmt sind;</li>
+                        <li>die Verarbeitung und Übertragung von personenbezogenen Daten für interne bzw. administrative Zwecke;</li>
+                        <li>die Vermeidung und Aufdeckung von Betrugsfällen und Straftaten;</li>
+                        <li>Gewährleistung der dauerhaften Funktionsfähigkeit unserer informationstechnologischen Systeme und der Technik unserer Internetseite mit dem Ziel, den Datenschutz und die Datensicherheit in unserem Unternehmen zu erhöhen.</li>
+                    </ul>
+                </li>
+            </ul>
+        </p>
+            
+        </div>
+
+        <div class="daten">
+            <h2>2 Ihre Rechte</h2>
+            <h3>1 Meine Rechte als betroffene Person</h3>
+            <p>Sie können unter der o. g. Adresse Auskunft über die zu Ihrer Person gespeicherten Daten verlangen (Art. 15 DSGVO). Darüber hinaus können Sie eine Berichtigung verlangen, wenn wir unrichtige Daten zu Ihrer Person gespeichert haben (Art. 16 DSGVO). Zudem können Sie unter bestimmten Voraussetzungen die Löschung Ihrer Daten verlangen (Art. 17 DSGVO) oder das Recht auf Widerspruch geltend machen (Art. 21 DSGVO). Ebenso steht Ihnen das Recht, auf Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu (Art. 18 DSGVO) sowie ein Recht auf Herausgabe der von Ihnen bereitgestellten Daten (Art. 20 DSGVO). Beim Auskunftsrecht und beim Löschungsrecht gelten die Einschränkungen nach §§ 34 und 35 BDSG. Darüber hinaus besteht ein Beschwerderecht bei einer Datenschutzaufsichtsbehörde (Artikel 77 DSGVO i. V. m § 19 BDSG).
+
+</p>
+            </div>
+
+            <div class="daten">
+            <h3>2 Widerspruch oder Widerruf gegen die Verarbeitung Ihrer Daten</h3>
+            <p>Falls Sie eine Einwilligung zur Verarbeitung Ihrer Daten erteilt haben, können Sie diese jederzeit widerrufen. Ein solcher Widerruf beeinflusst die Zulässigkeit der Verarbeitung Ihrer personenbezogenen Daten, nachdem Sie ihn gegenüber uns ausgesprochen haben.<br><br>
+Soweit wir die Verarbeitung Ihrer personenbezogenen Daten auf die Interessenabwägung stützen, können Sie Widerspruch gegen die Verarbeitung einlegen. Dies ist der Fall, wenn die Verarbeitung insbesondere nicht zur Erfüllung eines Vertrags mit Ihnen erforderlich ist, was von uns jeweils bei der nachfolgenden Beschreibung der Funktionen dargestellt wird. Bei Ausübung eines solchen Widerspruchs bitten wir um Darlegung der Gründe, weshalb wir Ihre personenbezogenen Daten nicht wie von uns durchgeführt verarbeiten sollten. Im Falle Ihres begründeten Widerspruchs prüfen wir die Sachlage und werden entweder die Datenverarbeitung einstellen bzw. anpassen oder Ihnen unsere zwingenden schutzwürdigen Gründe auf-zeigen, aufgrund derer wir die Verarbeitung fortführen.
+Selbstverständlich können Sie der Verarbeitung Ihrer personenbezogenen Daten für Zwecke der Werbung und Datenanalyse jederzeit widersprechen. <br><br>
+Über Ihren Werbewiderspruch können Sie uns unter folgenden Kontaktdaten informieren:<br><br>
+<b>Hoffmannoptik e.K.</b><br>
+Rebstraße 4<br>
+D-79395 Neuenburg am Rhein<br>
+Telefon 07631-73606<br>
+eMail: neuenburg@hoffmann-optik.de
+
+
+</p>
+            </div>
+            <div class="daten">
+            <h3>3 Wer bekommt meine Daten?</h3>
+            <p>Sofern in den detaillierten Beschreibungen der Angebote nicht abweichend geregelt, erhalten innerhalb unseres Unternehmens diejenigen Stellen Zugriff auf Ihre Daten, die diese zur Erfüllung unserer vertraglichen und gesetzlichen Pﬂichten brauchen. Informationen über Sie werden wir nur weitergeben, wenn gesetzliche Mitteilungspflichten dies gebieten, Sie eingewilligt haben und/oder ein anderer Rechtsgrund diese Weitergabe legitimiert.<br><br>
+Falls wir für einzelne Funktionen unseres Angebots auf beauftragte Dienstleister zurückgreifen wurden diese von uns sorgfältig ausgewählt und beauftragt, sind an unsere Weisungen gebunden und werden regelmäßig kontrolliert.
+Möchten wir Ihre Daten für werbliche Zwecke nutzen möchten, werden wir Sie untenstehend im Detail über die jeweiligen Vorgänge informieren.
+
+
+
+</p>
+            </div>
+            <div class="daten">
+            <h3>4 Wie lange werden meine Daten gespeichert?</h3>
+            <p>Falls dies in den detaillierten Beschreibungen der Angebote nicht abweichend geregelt ist, verarbeiten und speichern wir Ihre personenbezogenen Daten, solange es für die Erfüllung unserer vertraglichen und gesetzlichen Pﬂichten erforderlich ist.
+Regelmäßig gelöscht oder gesperrt werden Ihre personenbezogenen Daten, wenn sie für die Erfüllung vertraglicher oder gesetzlicher Pﬂichten nicht mehr erforderlich sind, sie von Ihrem Recht auf Löschung Gebrauch gemacht haben, sämtliche gegenseitige Ansprüche erfüllt sind und keine anderweitigen gesetzlichen Aufbewahrungspflichten oder gesetzliche Rechtfertigungsgrundlagen für die Speicherung bestehen.
+
+
+
+
+</p>
+            </div>
+
+            <div class="daten">
+            <h2>3 Erhebung personenbezogener Daten bei Besuch unserer Website</h2>
+            <h3>1 Verwendung von Server-Logfiles</h3>
+            <p>Mit jedem Aufruf der Internetseite durch eine betroffene Person oder ein automatisiertes System wird eine Reihe von allgemeinen Daten und Informationen in Logfiles erfasst. Hierzu gehören eine Internet-Protokoll-Adresse (IP-Adresse), die verwendeten Browsertypen und Versionen, die Internetseite, von welcher ein zugreifendes System auf unsere Internetseite gelangt (sogenannte Referrer), die Unterwebseiten, welche über ein zugreifendes System auf unserer Internetseite angesteuert werden, das Datum und die Uhrzeit eines Zugriffs auf die Internetseite und sonstige ähnliche Daten und Informationen, die der Gefahrenabwehr im Falle von Angriffen auf unsere informationstechnologischen Systeme dienen.<br><br>
+Rechtsgrundlage für die vorübergehende Speicherung der Daten und der Logfiles beruht auf Art. 6 Abs.1lit. f DSGVO mit den o. g. berechtigten Interessen.
+Eine vorübergehende Speicherung der IP-Adresse durch das System ist notwendig, um die Auslieferung der Website an den Rechner des Nutzers zu gewährleisten. Hierfür muss die IP-Adresse des Nutzers für die Dauer der Sitzung gespeichert bleiben.
+Die Speicherung in Logfiles erfolgt, um die Funktionsfähigkeit der Website sicherzustellen. Zudem dienen uns die Daten zur Optimierung der Website und zur Sicherstellung der Sicherheit unserer informationstechnischen Systeme. Auch hier liegt unser berechtigtes Interesse an der Datenverarbeitung nach Art. 6 Abs.1 lit. f DSGVO zugrunde. Die Daten werden gelöscht, sobald sie für die Erreichung des Zweckes ihrer Erhebung nicht mehr erforderlich sind. Im Falle der Erfassung der Daten zur Bereitstellung der Website ist dies der Fall, wenn die jeweilige Sitzung beendet ist. Die Erfassung der Daten zur Bereitstellung der Website und die Speicherung der Daten in Logfiles ist für den Betrieb der Internetseite erforderlich. Eine weitere Möglichkeit, zur Überprüfung der Logfiles besteht, wenn aufgrund konkreter Anhaltspunkte der berechtigte Verdacht einer rechtswidrigen Nutzung oder eines konkreten Angriffs auf unsere Webseite erfolgt. Unser berechtigtes Interesse die Verarbeitung liegt hier im Zwecke der Aufklärung und strafrechtlichen Verfolgung solcher Angriffe und rechtwidrigen Nutzungen.
+
+
+
+
+</p>
+            </div>
+
+            <div class="daten">
+            <h3>2 Verwendung von Cookies</h3>
+            <p>Zusätzlich zu den zuvor genannten Daten werden bei Ihrer Nutzung unserer Website Cookies auf Ihrem Rechner gespeichert. Bei Cookies handelt es sich um kleine Textdateien, die auf Ihrer Festplatte dem von Ihnen verwendeten Browser zugeordnet gespeichert werden und durch welche der Stelle, die den Cookie setzt (hier durch uns), bestimmte Informationen zufließen. Cookies können keine Programme ausführen oder Viren auf Ihren Computer übertragen. Sie dienen dazu, das Internetangebot insgesamt nutzerfreundlicher und effektiver zu machen.<br><br>
+Diese Website nutzt folgende Arten von Cookies, deren Umfang und Funktionsweise im Folgenden erläutert werden:<br><br>
+Transiente Cookies: Diese werden automatisiert gelöscht, wenn Sie den Browser schließen. Dazu zählen insbesondere die Session-Cookies. Diese speichern eine sogenannte Session-ID, mit welcher sich verschiedene Anfragen Ihres Browsers der gemeinsamen Sitzung zuordnen lassen. Dadurch kann Ihr Rechner wiedererkannt werden, wenn Sie auf unsere Website zurückkehren. Die Session-Cookies werden gelöscht, wenn Sie sich ausloggen oder den Browser schließen.
+Persistente Cookies: Diese werden automatisiert nach einer vorgegebenen Dauer gelöscht, die sich je nach Cookie unterscheiden kann. Sie können die Cookies in den Sicherheitseinstellungen Ihres Browsers jederzeit löschen.
+Beim Aufruf unserer Website werden Sie über die Verwendung von Cookies informiert und Ihre Einwilligung zur Verarbeitung der in diesem Zusammenhang verwendeten personenbezogenen Daten eingeholt. In diesem Zusammenhang erfolgt auch ein Hinweis auf diese Datenschutzerklärung. Sie können Ihre Browser-Einstellung entsprechend Ihren Wünschen konfigurieren und z. B. die Annahme von Third-Party-Cookies oder allen Cookies ablehnen. Dadurch haben Sie als Nutzer auch die volle Kontrolle über die Verwendung von Cookies. Die Rechtsgrundlage für die Verarbeitung personenbezogener Daten unter Verwendung von Cookies ist Art. 6 Abs.1 lit. f DSGVO. Werden Cookies für unsere Website deaktiviert, können möglicherweise nicht mehr alle Funktionen der Website vollumfänglich genutzt werden.
+
+
+
+
+</p>
+            </div>
+    </div>
+ </div>
+ <!-- TEXT SECTION END -->
+<?php get_footer(); ?>
